46620986c2ffb43102fc7c8926786f38aa29c16d
Rodrigo Tassinari de Oliveira <rodrigo@tassinari.eti.br>
2020-06-14 01:40:15 -0300

    Key pair tag operation

--- lib/cheap_machines/operations/key_pair/tag.rb
+++ lib/cheap_machines/operations/key_pair/tag.rb
@@ -4,9 +4,44 @@ module CheapMachines
       class Tag
         include Dry::Transaction::Operation
 
+        # Adds a project tag to the key_pair, if needed
+        #
+        # @param input [Hash] the input (only relevant keys documented below)
+        # @option input [Aws::EC2::Client] :ec2_client The configured AWS EC2 client
+        # @option input [Logger] :logger The logger
+        # @option input [Aws::EC2::Types::KeyPairInfo] :key_pair The key_pair
+        # @option input [String] :project_name The project name
+        # @return [Dry::Monads::Success] A success object with the same input as output
+        # @return [Dry::Monads::Failure] A failure object with a error message string if the operation failed
         def call(input)
-          # returns Success(valid_data) or Failure(validation)
-          Success(input) # TODO
+          ec2 = input[:ec2_client]
+          logger = input[:logger]
+          project_name = input[:project_name]
+          # https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/EC2/Types/KeyPairInfo.html
+          key_pair = input[:key_pair]
+
+          unless has_project_tag?(key_pair, project_name)
+            add_project_tag(ec2, key_pair, project_name)
+          end
+
+          Success(input)
+        rescue Aws::Errors::ServiceError, Aws::Errors::NoSuchEndpointError => exception
+          Failure("error connecting to AWS: #{exception.inspect}")
+        end
+
+        private
+
+        def has_project_tag?(key_pair, project_name)
+          # https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/EC2/Types/Tag.html
+          key_pair.tags.detect { |t| t.key == "ProjectName" }&.value == project_name
+        end
+
+        def add_project_tag(ec2, key_pair, project_name)
+          # https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/EC2/Client.html#create_tags-instance_method
+          ec2.create_tags(
+            resources: [key_pair.key_pair_id],
+            tags: [{key: "ProjectName", value: project_name}]
+          )
         end
 
       end
--- test/cheap_machines/operations/key_pair/tag_test.rb
+++ test/cheap_machines/operations/key_pair/tag_test.rb
@@ -5,7 +5,79 @@ describe CheapMachines::Operations::KeyPair::Tag do
   let(:subject) { described_class.new }
 
   describe "#call" do
+    let(:ec2_client) { TestHelpers.ec2_client }
+    let(:logger) { TestHelpers.logger }
+    let(:key_pair) { stub("Aws::EC2::Types::KeyPairInfo", key_pair_id: "some-id", tags: []) }
+    let(:input) { {ec2_client: ec2_client, logger: logger, key_pair: key_pair, project_name: "name-of-project"} }
     
-  end
+    describe "ec2 client error" do
+      before do
+        # https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/EC2/Errors.html
+        ec2_client.stub_responses(:create_tags, Aws::EC2::Errors::AuthFailure.new({}, "invalid credentials"))
+      end
+      it "returns a failure with error message" do
+        result = subject.call(input)
+        _(result).must_be_kind_of(::Dry::Monads::Result)
+        _(result.failure?).must_equal(true)
+        _(result.failure).must_equal("error connecting to AWS: #<Aws::EC2::Errors::AuthFailure: invalid credentials>")
+      end
+    end
 
+    describe "key_pair doesn't have the ProjectName tag" do
+      before do
+        ec2_client.stub_responses(:create_tags, {})
+      end
+      it "tags the key pair" do
+        ec2_client_spy = Spy.on(ec2_client, :create_tags).and_call_through
+        result = subject.call(input)
+        assert ec2_client_spy.has_been_called_with?(
+          resources: ["some-id"],
+          tags: [{key: "ProjectName", value: "name-of-project"}]
+        )
+      end
+      it "returns a success" do
+        result = subject.call(input)
+        _(result).must_be_kind_of(::Dry::Monads::Result)
+        _(result.success?).must_equal(true)
+      end
+    end
+
+    describe "key_pair has a different ProjectName tag" do
+      let(:key_pair) { stub("Aws::EC2::Types::KeyPairInfo", key_pair_id: "some-id", tags: [Aws::EC2::Types::Tag.new(key: "ProjectName", value: "other-project")]) }
+      before do
+        ec2_client.stub_responses(:create_tags, {})
+      end
+      it "tags the key pair" do
+        ec2_client_spy = Spy.on(ec2_client, :create_tags).and_call_through
+        result = subject.call(input)
+        assert ec2_client_spy.has_been_called_with?(
+          resources: ["some-id"],
+          tags: [{key: "ProjectName", value: "name-of-project"}]
+        )
+      end
+      it "returns a success" do
+        result = subject.call(input)
+        _(result).must_be_kind_of(::Dry::Monads::Result)
+        _(result.success?).must_equal(true)
+      end
+    end
+
+    describe "key_pair already has the current ProjectName tag" do
+      let(:key_pair) { stub("Aws::EC2::Types::KeyPairInfo", key_pair_id: "some-id", tags: [Aws::EC2::Types::Tag.new(key: "ProjectName", value: "name-of-project")]) }
+      before do
+        ec2_client.stub_responses(:create_tags, {})
+      end
+      it "does not tag the key pair" do
+        ec2_client_spy = Spy.on(ec2_client, :create_tags)
+        result = subject.call(input)
+        refute ec2_client_spy.has_been_called?
+      end
+      it "returns a success" do
+        result = subject.call(input)
+        _(result).must_be_kind_of(::Dry::Monads::Result)
+        _(result.success?).must_equal(true)
+      end
+    end
+  end
+    
 end
